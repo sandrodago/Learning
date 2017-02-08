5870dabfc316b720c730a3421551568a55db71ed
sandrodago <dagostino.alex@gmail.com>
2017-02-08 03:19:02 -0500

    webapge reloads on html, js or css file-save.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'); // get the gulp commands
+var sass = require('gulp-sass'); // be able to translat scss to css
+var browserSync = require('browser-sync').create(); // get the browser sync service
 
 gulp.task('hello', function() {
 	console.log("Hello, Alex!");
@@ -9,10 +10,26 @@ gulp.task('sass', function() {
 	return gulp.src('app/scss/**/*.scss')
 		.pipe(sass()) //Using gulp-sass
 		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
-// Gulp watch syntax
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
+// Gulp watch syntax, allows function to keep running on file-save.
+gulp.task('watch', ['browserSync', 'sass'], function(){ 
+  // browserSync is in an array of tasks.
+  // that must be completed before watch is allowed to run.
+  gulp.watch('app/scss/**/*.scss', ['sass']);  
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
   // Other watchers
+})
+
+// browser sync needs to know where the server is to continuously spin the page.
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
